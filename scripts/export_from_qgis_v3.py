d2455cde05e5f92460f7a90f52c486e8f310f7ff
edigonzales <edi.gonzales@gmail.com>
2014-09-03 20:40:54 +0200

    Testscripts

--- scripts/export_from_qgis_v3.py
+++ scripts/export_from_qgis_v3.py
@@ -2,27 +2,15 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from qgis.core import *
 from qgis.gui import *
-import qgis.utils
-
-
-
 tileindex = "/home/stefan/Projekte/map_strassenkarte_ng/data/tileindex/dtm_solo_01.shp"
 dpi = 508
 scale = 10000
 lst = []
-
-root = QgsProject.instance().layerTreeRoot()
-
 layers = qgis.utils.iface.legendInterface().layers()
 for layer in layers:
-    node = root.findLayer(layer.id())
-    if node.isVisible():
-        lst.append(layer.id());
-    
+    lst.append(layer.id())
     
 print lst
-
-
 vlayer = QgsVectorLayer(tileindex, "tileindex", "ogr")
 iter = vlayer.getFeatures()
 for feature in iter:
@@ -30,24 +18,19 @@ for feature in iter:
     p1 = geom.vertexAt(0)
     p2 = geom.vertexAt(2)
     rect = QgsRectangle(p1,  p2) # wird normalisiert
-    
     dx = rect.width()
     dy = rect.height()
-
     width = (dx/scale) / 0.0254 * dpi
     height = (dy/scale) / 0.0254 * dpi
     print height
-
     filename = "strassenkarte_ng_"+str(scale)+"_"+str(int(rect.xMinimum()))+"_"+str(int(rect.yMaximum()))
     print filename    
-    
     # Rasterfile exportieren
     img = QImage(QSize(width,  height), QImage.Format_RGB32)
 #    img = QImage(QSize(width,  height), QImage.Format_ARGB32)  
 #    img = QImage(QSize(width, height), QImage.Format_ARGB32_Premultiplied)
     img.setDotsPerMeterX(dpi / 25.4 * 1000)
     img.setDotsPerMeterY(dpi / 25.4 * 1000)
-    
     color = QColor(255,255,255)
     img.fill(color.rgb())
     p = QPainter()
@@ -60,17 +43,9 @@ for feature in iter:
     render.setOutputSize(img.size(), dpi)
     render.render(p)
     p.end()
-    
     print render.outputDpi()
-    print render.mapUnits()
-    
+    print render.mapUnits()    
     img.save("/tmp/"+filename+".png","png")    
-
-#    imgwriter = QImageWriter("/tmp/"+filename+".tif",  "tiff")
-#    imgwriter.setCompression(1)
-#    imgwriter.write(img)
-    
-    # Worldfile speichern
     res = dx/width
     with open("/tmp/"+filename+".pngw", 'w') as outfile:
         outfile.write(str(res))
